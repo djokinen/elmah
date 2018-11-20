eba196ddd7c614e8aee253c8be053977da8e4d1e
dan-axiom <dan.jokinen@axiomgroup.ca>
2018-11-19 22:20:17 -0500

    init commit
    ....

---
+++ www/default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/core.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="www._default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+	<h2>Application Names</h2>
+	<asp:DropDownList ID="applicationNames" runat="server"></asp:DropDownList>
+	<h2>Error List</h2>
+	<asp:GridView ID="grid" runat="server"></asp:GridView>
+</asp:Content>
---
+++ www/default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace www
+{
+	public partial class _default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			using (var entities = new ErrorLogEntities())
+			{
+				applicationNames.DataSource = entities.ELMAH_Error.Select(s => s.Application).Distinct().ToList();
+				applicationNames.DataBind();
+
+				grid.DataSource = entities.ELMAH_Error.ToList();
+				grid.DataBind();
+			}
+		}
+	}
+}
